b68ac8f16b5ed8e1db611bae122e150f85e628d3
Thibaut792 <thibautfiquet7@gmail.com>
2023-03-20 17:03:20 +0100

    add userPool cognito and auth form

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from './aws-exports';
+import awsExports from './CognitoConfiguration';
 Amplify.configure(awsExports);
 
 export default function App() {
   return (
     <Authenticator>
-      {({ signOut, user }) => (
+      {({ signOut, user }) => {
+        console.log("user", user)
+        console.log("signOut", signOut)
+        return(
         <main>
           <h1>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
         </main>
-      )}
+      )}}
     </Authenticator>
   );
 }
