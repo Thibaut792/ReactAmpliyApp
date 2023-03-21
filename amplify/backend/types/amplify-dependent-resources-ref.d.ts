4657aa909cb7efbeb8f585c0a4e9e5e081f2fcc7
Thibaut792 <thibautfiquet7@gmail.com>
2023-03-21 16:43:35 +0100

    essai amplify add storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifypushs3f9e45267": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3Upload": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import { Amplify } from 'aws-amplify';
 
 import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 
+import '@aws-amplify/ui-react/styles.css';
+import { Storage } from 'aws-amplify';
 import awsExports from './CognitoConfiguration';
+import { useState } from "react";
 Amplify.configure(awsExports);
 
-export default function App() {
+// export default function App(signOut, user ) {
+
+
+//   // return (
+    
+//   //     {({ signOut, user }) => {
+//   //       console.log("user", user)
+//   //       console.log("signOut", signOut)
+//   //       return(
+//   //       <main>
+//   //         <h1>Hello {user.username}</h1>
+//   //         <button onClick={signOut}>Sign out</button>
+//   //       </main>
+//   //     )}}
+
+//   // );
+// }
+
+function App({ signOut, user }) {
+  const [fileData, setFileData] = useState();
+  const [fileStatus, setFileStatus] = useState(false);
+
+  const uploadFile = async () => {
+    const result = await Storage.put(fileData.name, fileData, {
+      contentType: fileData.type,
+    });
+    setFileStatus(true);
+    console.log(21, result);
+  };
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => {
-        console.log("user", user)
-        console.log("signOut", signOut)
-        return(
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}}
-    </Authenticator>
+    <div className="App">
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+
+      <div>
+        <input type="file" onChange={(e) => setFileData(e.target.files[0])} />
+      </div>
+      <div>
+        <button onClick={uploadFile}>Upload file</button>
+      </div>
+      {fileStatus ? "File uploaded successfully" : ""}
+    </div>
   );
 }
+
+export default Authenticator(App);
