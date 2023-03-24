754523da97631e40a8c2e7fb0905ef871d9ae28c
Thibaut792 <thibautfiquet7@gmail.com>
2023-03-24 14:37:35 +0100

    upload doc (axios)

--- src/App.js
+++ src/App.js
@@ -4,50 +4,33 @@ import { uploadFile } from 'react-s3';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './Components/CognitoConfiguration';
+import Uploader from './Components/Uploader';
 
 Amplify.configure(awsExports);
 
-// const S3_BUCKET ='thibaut-aws-learning';
-// const REGION ='eu-west-3';
 
-// const config = {
-//   bucketName: S3_BUCKET,
-//   region : REGION,
+// export default function App() {
+//   return (
+//     <Authenticator>
+//       {({ signOut, user }) => {
+//         return(
+//         <main>
+//           <h1>Bienvenue sur amplify {user.username}</h1>
+//           <button onClick={signOut}>Se déconnecter</button>
+//         </main>
+//       )}}
+//     </Authenticator>
+//   );
 // }
 
-// const UploadImageToS3WithReactS3 = () => {
-   
-//   const [selectedFile, setSelectedFile] = useState(null);
-
-//   const handleFileInput = (e) => {
-//     setSelectedFile(e.target.files[0]);
-//   }
-
-//   const handleUpload = async (file) => {
-//     uploadFile(file, config)
-//         .then(data => console.log(data))
-//         .catch(err => console.error(err))
-//   }
-
-//   return <div>
-//         <div>React S3 File Upload</div>
-//         <input type="file" onChange={handleFileInput}/>
-//         <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
-//     </div>
-// }
-
-// export default UploadImageToS3WithReactS3;
-
-export default function App() {
-  return (
-    <Authenticator>
-      {({ signOut, user }) => {
-        return(
-        <main>
-          <h1>Bienvenue sur amplify {user.username}</h1>
-          <button onClick={signOut}>Se déconnecter</button>
-        </main>
-      )}}
-    </Authenticator>
-  );
+function App(){
+  return(
+    <div className="App">
+      <meta charSet="utf-8"/>
+      <h1>Upload Document to amazon S3</h1>
+      <Uploader />
+    </div>
+  )
 }
+
+export default App;
---
+++ src/Components/Uploader.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import 'react-dropzone-uploader/dist/styles.css'
+import Dropzone from 'react-dropzone-uploader';
+
+const Uploader = () => {
+
+    const axios = require('axios');
+    const API_ENDPOINT = "https://3s5er1ux98.execute-api.eu-west-3.amazonaws.com/thibaut_test-presigned-url";
+    const handleChangeStatus = ({ meta, remove }, status) => {
+      console.log(status, meta);
+    };
+
+    const handleSubmit = async (files) => {
+        const f = files[0];
+        console.log(f);
+
+        const response = await axios({
+            method: 'GET',
+            url: API_ENDPOINT
+          });
+
+        console.log("Response", response);
+
+        const result = await fetch(response.data.uploadURL, {
+          method: 'PUT',
+          headers:{
+            "Content-Type": "image/jpeg"
+          },
+          body: f[files]
+        })
+        console.log('Result: ', result)
+    };
+    
+  
+    return (
+        <Dropzone
+        //   getUploadParams={getUploadParams}
+          onChangeStatus={handleChangeStatus}
+          onSubmit={handleSubmit}
+          maxFiles={3}
+          multiple={false}
+          canCancel={false}
+          inputContent="Deposer votre document"
+          styles={{
+            dropzone: { width: 400, height: 200 },
+            dropzoneActive: { borderColor: 'green' },
+          }}
+        />
+    )
+  }
+  
+  <Uploader />
+
+export default Uploader
